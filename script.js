f7e2b02a7aebb0c64be538b78e898c8b33f14bf8
freylaura <123412371+freylaura@users.noreply.github.com>
2023-01-29 20:24:24 +0100

    changed api url

--- script.js
+++ script.js
@@ -57,11 +57,11 @@ function handleSubmit(event) {
 }
 
 let standort = document.querySelector("#searchForm");
-standort.addEventListener("click", handleSubmit);
+standort.addEventListener("submit", handleSubmit);
 
 function searchLocation(position) {
   let apiKey = "ca5af28648d86b7925348bb9fb85cd3a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.langitude}&lon=${position.coords.longitude}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
 function showLocation(event) {
